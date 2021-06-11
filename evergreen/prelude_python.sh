d936150f40840328e7d6c76acbcdf949d77ca55c
Daniel Moody <daniel.moody@mongodb.com>
2021-06-10 22:10:22 -0500

    fix comment

--- evergreen/prelude_python.sh
+++ evergreen/prelude_python.sh
@@ -4,7 +4,7 @@ else
   if [ -f /opt/mongodbtoolchain/v3/bin/python3 ]; then
     python="/opt/mongodbtoolchain/v3/bin/python3"
   else
-    echo "Could find toolchain python, using system python $(which python3)"
+    echo "Could not find mongodbtoolchain python, using system python $(which python3)"
     python=`which python3`
   fi
 fi
